a16db2160bd9729a48c26e3ef446069916201e6a
victor_ferreira <vitorsantosferreira10@gmail.com>
2025-06-06 15:08:32 -0300

    feat: integrate customer route into express setup and add test for customer creation failure

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize-typescript'
 import express, { Express } from 'express'
 import CustomerModel from '../costumer/repository/sequelize/customer.model'
+import { customerRoute } from './routes/customer.route'
 
 export const app: Express = express()
 
 app.use(express.json())
+app.use('/customer', customerRoute)
 
 export let sequelize: Sequelize
 
---
+++ src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, Router } from 'express'
+import CreateCustomerUseCase from '../../../usecase/customer/create/create.customer.usecase'
+import CustomerRepository from '../../costumer/repository/sequelize/costumer.repository'
+
+export const customerRoute: Router = express.Router()
+
+customerRoute.post('/', async (req: Request, res: Response) => {
+  const usecase = new CreateCustomerUseCase(new CustomerRepository())
+  try {
+    const customerDto = {
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        number: req.body.address.number,
+        zip: req.body.address.zip,
+        city: req.body.address.city,
+      },
+    }
+
+    const output = await usecase.execute(customerDto)
+    res.send(output)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
